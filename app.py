db5f0fe9094dc379c7a839203c837be79438264f
alt-nikitha <nikithasrikanth1711@gmail.com>
2020-09-18 11:56:21 +0530

    root page

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ def index():
     else:
         return render_template('index.html',text="")
 
-
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
 if __name__ == "__main__":
     
     app.run(debug=False)
